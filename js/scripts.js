f44261f95c883b94d73e57f89d802ebabb280272
ninzy <nikitkatigr@gmail.com>
2020-10-04 23:11:53 +0300

    menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('nav .menu-btn .line--1');
+var lineTwo = document.querySelector('nav .menu-btn .line--2');
+var lineThree = document.querySelector('nav .menu-btn .line--3');
+var link = document.querySelector('nav .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
+
 $( function() {
     $( "#dialog" ).dialog({
         autoOpen: false,
@@ -10,14 +24,34 @@ $( function() {
             duration: 1000
         }
     });
+    $(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+            loop:true,
+            margin:0,
+            nav:true,
+            navText: [ '', ' ' ],
+
+            responsive:{
+                0:{
+                    items:1
+                },
+
+                1000:{
+                    items:1
+                }
+            }
+        });
+    });
 
     $( "#opener" ).on( "click", function() {
         $( "#dialog" ).dialog( "open" );
     });
 } );
-
 $( "#dialog" ).dialog({
-    draggable: false},
+        draggable: false},
     {
-    resizable: false
-});
+        resizable: false
+    });
+
+
+
